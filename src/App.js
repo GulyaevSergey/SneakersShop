84f4fe033c20609f7de0029375189d7244c73e0d
GulyaevSergey <109235648+GulyaevSergey@users.noreply.github.com>
2023-01-04 00:01:24 +0700

    init

--- src/App.js
+++ src/App.js
@@ -1,25 +1,148 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="wrapper">
+            <header className="header">
+                <div className="headerLeft">
+                    <img
+                        width={40}
+                        height={40}
+                        src="/img/logo.png"
+                        alt="logo"
+                    />
+                    <div>
+                        <h3 className="logoName">Sneakers Shop</h3>
+                        <p>Магазин лучших кроссовок</p>
+                    </div>
+                </div>
+                <ul className="headerRight">
+                    <li>
+                        <img
+                            width={18}
+                            height={18}
+                            src="/img/cart.svg"
+                            alt="cart"
+                        />
+                        <span>1205 руб.</span>
+                    </li>
+                    <li>
+                        <img
+                            width={18}
+                            height={18}
+                            src="/img/favorite.svg"
+                            alt="favorite"
+                        />
+                    </li>
+                    <li>
+                        <img
+                            width={18}
+                            height={18}
+                            src="/img/user.svg"
+                            alt="user"
+                        />
+                    </li>
+                </ul>
+            </header>
+            <div className="content">
+                <h1>Все кроссовки</h1>
+
+                <div className="sneakers">
+                    <div className="card">
+                        <img
+                            width={133}
+                            height={112}
+                            src="/img/sneakers/Nike_blazer_Mid Suede Green.jpg"
+                            alt="Nike blazer Mid Suede Green"
+                        />
+                        <h5>Мужские кроссовки Nike blazer Mid Suede</h5>
+                        <div className="cartInfo">
+                            <div className="cartPrice">
+                                <span>Цена:</span>
+                                <b>12 999 руб.</b>
+                            </div>
+                            <button className="button">
+                                <img
+                                    width={11}
+                                    height={11}
+                                    src="/img/plus.svg"
+                                    alt="plus"
+                                />
+                            </button>
+                        </div>
+                    </div>
+                    <div className="card">
+                        <img
+                            width={133}
+                            height={112}
+                            src="/img/sneakers/Nike Air Max 270.jpg"
+                            alt="Nike blazer Mid Suede Green"
+                        />
+                        <h5>Мужские кроссовки Nike Air Max 270</h5>
+                        <div className="cartInfo">
+                            <div className="cartPrice">
+                                <span>Цена:</span>
+                                <b>12 999 руб.</b>
+                            </div>
+                            <button className="button">
+                                <img
+                                    width={11}
+                                    height={11}
+                                    src="/img/plus.svg"
+                                    alt="plus"
+                                />
+                            </button>
+                        </div>
+                    </div>
+                    <div className="card">
+                        <img
+                            width={143}
+                            height={112}
+                            src="/img/sneakers/Nike blazer Mid Suede White.jpg"
+                            alt="Мужские кроссовки Nike blazer Mid Suede"
+                        />
+                        <h5>Мужские кроссовки Nike blazer Mid Suede</h5>
+                        <div className="cartInfo">
+                            <div className="cartPrice">
+                                <span>Цена:</span>
+                                <b>8 499 руб.</b>
+                            </div>
+                            <button className="button">
+                                <img
+                                    width={11}
+                                    height={11}
+                                    src="/img/plus.svg"
+                                    alt="plus"
+                                />
+                            </button>
+                        </div>
+                    </div>
+                    <div className="card">
+                        <img
+                            width={143}
+                            height={112}
+                            src="/img/sneakers/Puma X Aka Boku Future Rider.jpg"
+                            alt="Puma X Aka Boku Future Rider"
+                        />
+                        <h5>Мужские кроссовки Puma X Aka Boku Future Rider</h5>
+                        <div className="cartInfo">
+                            <div className="cartPrice">
+                                <span>Цена:</span>
+                                <b>8 999 руб.</b>
+                            </div>
+                            <button className="button">
+                                <img
+                                    width={11}
+                                    height={11}
+                                    src="/img/plus.svg"
+                                    alt="plus"
+                                />
+                            </button>
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+        </div>
+    );
 }
 
 export default App;
